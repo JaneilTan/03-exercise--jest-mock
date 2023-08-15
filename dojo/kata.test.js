0cd7441da71a0845f483871f0118cf4cc9081fbe
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-08-16 10:40:11 +1200

    used jest mock function

--- dojo/kata.test.js
+++ dojo/kata.test.js
@@ -1,3 +1,21 @@
-const {} = require("./kata");
+const { goVroom } = require("./kata");
+const faker = require("faker");
 
-describe("Dojo", () => {});
+jest.mock("faker", () => ({
+  datatype: {
+    number: jest.fn(),
+  },
+}));
+
+describe("Dojo", () => {
+    it('should return a string', () =>{
+        faker.datatype.number.mockReturnValue(5);
+
+    const car = { make: "Mazda", model: "XR8" };
+    const expectedVroom = "The Mazda XR8 goes vroooooom!!";
+
+    const result = goVroom(car);
+
+    expect(result).toBe(expectedVroom);
+    });
+});
